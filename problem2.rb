3c15908279b7bc55992644ff3b8acc1e5f0ab882
Joshua Meadows <joshuakmeadows@gmail.com>
2018-06-13 23:32:12 +0000

    Added death stuff

--- problem2.rb
+++ problem2.rb
@@ -1,1 +1,17 @@
 puts "Hello Moto"
+
+#Data
+
+dead_people = ["Ethel", "Mortimer", "Buford"]
+alive_people = ["Kelly", "Joe", "Megan"]
+sick_people = alive_people.slice(0,1)
+
+#commands
+#
+alive_people.each do |x|
+    if sick_people.include? x
+        next
+    else
+    puts "#{x} is to receive a questionnaire"
+    end
+end
